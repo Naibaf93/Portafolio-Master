c9d5204e74fbdaef22508fef79cffa699eead4e4
Fabián Huertas <miloh9318@hotmail.com>
2024-10-26 16:32:22 -0500

    toggle de iconos en el menu responsive

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
     let btn = document.querySelector(".layout__menu-toggle");
+    let icon__bars = document.querySelector(".layout__menu-toggle .fa-bars");
+    let icon__xmark = document.querySelector(".layout__menu-toggle .fa-xmark");
     let aside = document.querySelector(".layout__aside");
 
     btn.addEventListener("click", (event) => {
@@ -9,8 +11,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         if(!visible){
             aside.classList.add("layout__aside--visible");
+            icon__bars.style.opacity = 0;
+            icon__xmark.style.opacity = 1;
         } else{
             aside.classList.remove("layout__aside--visible"); 
+            icon__bars.style.opacity = 1;
+            icon__xmark.style.opacity = 0;
         }
     });
 });
